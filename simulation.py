c05a362f6e5df4e36a520b409ac29e0a38a664d4
schromya <schromya@gmail.com>
2024-11-09 16:10:06 -0600

    Tried out torque control on joint 2

--- simulation.py
+++ simulation.py
@@ -4,7 +4,7 @@ import pybullet_data
 
 physics_client = p.connect(p.GUI) # or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
-p.setGravity(0, 0, 0) # No gravity since we are doing our own gravity dynamics
+p.setGravity(0, 0, -9.81)
 
 # Load ground
 plane_ID = p.loadURDF("plane.urdf")
@@ -13,6 +13,22 @@ plane_ID = p.loadURDF("plane.urdf")
 start_pos = [0,0,0]
 start_orientation = p.getQuaternionFromEuler([0,0,0])
 robot = p.loadURDF("panda_arm.urdf", start_pos, start_orientation)
+
+num_joints = p.getNumJoints(robot)
+
+torque = 5.0
+
+p.setJointMotorControl2(
+    bodyIndex=robot,
+    jointIndex=2, 
+    controlMode=p.TORQUE_CONTROL,
+    force=torque
+)
+
+# Step the simulation for a while to observe the effect
+for _ in range(1000):
+    p.stepSimulation() # Default is 1/240hz
+    time.sleep(1/240)  # Match step simulation
 
 # Keep simulation up to see URDF
 while(True):
